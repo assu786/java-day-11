2ffe3ad38a58753f5728e7b4e503f6201cbb327b
assu786 <shaikaqsa.78@gmail.com>
2024-12-12 15:01:42 +0530

    Add files via upload

---
+++ contains.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class a 
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> b=new ArrayList<>();
+        b.add(645);
+        b.add(467);
+        b.add(878);
+        System.out.println(b.contains(645));
+        System.out.println(b.contains(886));
+        
+    }
+}
