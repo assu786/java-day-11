c9c2200f67d9b54c0bbe14c6fa42a29acd169fbe
assu786 <shaikaqsa.78@gmail.com>
2024-12-12 11:20:25 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class d
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(453);
+        a.add(465);
+        a.add(758);
+        System.out.println(a);
+    }
+}
