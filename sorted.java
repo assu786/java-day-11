a245a7c7284b8cf96ef061fbe7e210ccf079915e
assu786 <shaikaqsa.78@gmail.com>
2024-12-12 12:23:35 +0530

    Add files via upload

---
+++ sorted.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class d
+{
+    public static void main(String args[])
+    {
+        ArrayList <Integer> a=new ArrayList<>();
+        a.add(45);
+        a.add(77);
+        a.add(12);
+        a.add(87);
+        Collections.sort(a);
+        System.out.println(a);
+    }
+}
