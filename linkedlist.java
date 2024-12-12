f593543857b1c7dfbf4aacd93bc01265a902d8ff
assu786 <shaikaqsa.78@gmail.com>
2024-12-12 15:20:00 +0530

    Add files via upload

---
+++ linkedlist.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class a 
+{
+    public static void main(String args[])
+    {
+        LinkedList<Integer> a=new LinkedList<>();
+        a.add(456);
+        a.add(746);
+        a.add(876);
+        System.out.println(a);
+        a.addFirst(645);
+        System.out.println(a);
+        a.addLast(575);
+        System.out.println(a);
+
+        
+    }
+}
