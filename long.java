f543520086f7cf6a6e816a1579bfaa955e8a84db
assu786 <shaikaqsa.78@gmail.com>
2024-12-12 13:57:16 +0530

    Add files via upload

---
+++ long.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class add{
+    public static void main(String args[])
+    {
+        long ans=80;
+        Scanner sc=new Scanner(System.in);
+        long s=sc.nextLong();
+        System.out.println(ans);
+        System.out.println(s);
+    }
+}
