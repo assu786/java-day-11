61d9ca9e84c464aa2e7c50f3e028c2dcffe268c9
assu786 <shaikaqsa.78@gmail.com>
2024-12-12 10:27:45 +0530

    Add files via upload

---
+++ func.java
@@ -0,0 +1,15 @@
+import java.util.*;
+interface math
+{
+    public int operation(int a,int b);
+}
+class C 
+{
+    public static void main(String args[])
+    {
+        math add=(a,b)->a+b;
+        math sub=(a,b)->a-b;
+        System.out.println(add.operation(4,5));
+        System.out.println(sub.operation(10,7));
+    }
+}
